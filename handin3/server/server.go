c710d714a7f6eb526f65ed9b56551b1e8f682547
katrinesando <kmsa@itu.dk>
2022-10-12 14:44:07 +0200

    set up client and server base

---
+++ handin3/server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	t "time"
+
+	pb "github.com/katrinesando/Distributed-Systems/tree/ChittyChat_Handin3/proto"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedGetCurrentTimeServer
+}
+
+func (s *Server) GetTime(ctx context.Context, in *pb.GetTimeRequest) (*pb.GetTimeReply, error) {
+	fmt.Printf("Received GetTime request\n")
+	return &pb.GetTimeReply{Reply: t.Now().String()}, nil
+}
+
+func main() {
+	// Create listener tcp on port 9080
+	list, err := net.Listen("tcp", ":9080")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9080: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGetCurrentTimeServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
+}
