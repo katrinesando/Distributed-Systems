bebbdeb63b76085da908518a151489832a52691c
Maxime Birkkjær Havez <maxih@hotmail.dk>
2022-10-26 12:33:28 +0200

    Removed unnecessary folder and moved useful content to main folder

--- handin3/client/client.go
+++ handin3/client/client.go
@@ -1,25 +1,115 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
 	h "handin3"
+	"io"
 	"log"
+	"os"
 
 	"google.golang.org/grpc"
+	"wesionary.team/dipeshdulal/console-chat/chatpb"
 )
 
-var id int
+var channelName = flag.String("channel", "default", "Channel name for chatting")
+var senderName = flag.String("sender", "default", "Senders name")
+var tcpServer = flag.String("server", ":9100", "Tcp server")
 var clock h.Vector
 
-func main() {
-	// Creat a virtual RPC Client Connection on port  9080 WithInsecure (because  of http)
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(":9080", grpc.WithInsecure())
+func joinChannel(ctx context.Context, client chatpb.ChatServiceClient) {
+
+	channel := chatpb.Channel{Name: *channelName, SendersName: *senderName}
+
+	stream, err := client.JoinChannel(ctx, &channel)
 	if err != nil {
-		log.Fatalf("Could not connect: %s", err)
+		log.Fatalf("couldnt connect to client: %v", err)
 	}
 
-	// Defer means: When this function returns, call this method (meaing, one main is done, close connection)
+	// fmt.Printf("Joined channel")
+
+	waitc := make(chan struct{}) //wait for something to finish, requires almost no memory
+
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err != nil {
+				log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
+			}
+			if err == io.EOF { //done sending message
+				close(waitc)
+				return
+			}
+
+			if *senderName != in.Sender {
+				mes := fmt.Sprintf("%v", in.Message)
+				if mes == "4040" {
+					fmt.Printf("--- %v Left the chat ---\n", in.Sender)
+				} else if mes == "1111" {
+					fmt.Printf("--- %v Joined the Chat ---\n", in.Sender)
+				} else {
+					fmt.Printf("(%v) : %v \n", in.Sender, in.Message)
+				}
+			}
+		}
+	}()
+	<-waitc
+}
+
+func sendMessage(ctx context.Context, client chatpb.ChatServiceClient, message string) {
+
+	stream, err := client.SendMessage(ctx)
+	if err != nil {
+		log.Printf("Cannot send message: error: %v", err)
+	}
+	msg := chatpb.Message{
+		Channel: &chatpb.Channel{
+			Name:        *channelName,
+			SendersName: *senderName},
+		Message: message,
+		Sender:  *senderName,
+	}
+	stream.Send(&msg)
+
+	//sends message to console
+	ack, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Print("Cannot send ack: %v", err)
+		log.Print(ack)
+	}
+
+}
+
+func main() {
+
+	flag.Parse()
+
+	fmt.Println("┌─────────────────────────────┐")
+	fmt.Println("│            		      │")
+	fmt.Println("│ Welcome to the Chat Service │")
+	fmt.Println("│            		      │")
+	fmt.Println("└─────────────────────────────┘")
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
+
+	conn, err := grpc.Dial(*tcpServer, opts...)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
 	defer conn.Close()
+
+	ctx := context.Background()
+	client := chatpb.NewChatServiceClient(conn)
+
+	go joinChannel(ctx, client)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		go sendMessage(ctx, client, scanner.Text())
+	}
 
 }
 
--- handin3/server/server.go
+++ handin3/server/server.go
@@ -1,39 +1,123 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	h "handin3"
+	"io"
 	"log"
 	"net"
-	t "time"
-
-	pb "github.com/katrinesando/Distributed-Systems/tree/ChittyChat_Handin3/proto"
+	"sync"
 
 	"google.golang.org/grpc"
+	"wesionary.team/dipeshdulal/console-chat/chatpb"
 )
-
-type Server struct {
-	pb.UnimplementedGetCurrentTimeServer
-}
 
 var clock h.Vector
 
-func (s *Server) GetTime(ctx context.Context, in *pb.GetTimeRequest) (*pb.GetTimeReply, error) {
-	fmt.Printf("Received GetTime request\n")
-	return &pb.GetTimeReply{Reply: t.Now().String()}, nil
+type server struct {
+	chatpb.UnimplementedChatServiceServer
+	mu      sync.Mutex
+	channel map[string][]chan *chatpb.Message
+}
+
+func (s *server) JoinChannel(ch *chatpb.Channel, msgStream chatpb.ChatService_JoinChannelServer) error {
+	msgChannel := make(chan *chatpb.Message)
+	s.channel[ch.Name] = append(s.channel[ch.Name], msgChannel)
+	log.Printf("--------- %v joined Chat ----------\n", ch.SendersName)
+
+	msg := chatpb.Message{
+		Channel: &chatpb.Channel{
+			Name:        ch.Name,
+			SendersName: ch.SendersName},
+		Message: "1111",
+		Sender:  ch.SendersName,
+	}
+
+	go func() {
+		streams := s.channel[msg.Channel.Name]
+		for _, msgChan := range streams {
+			msgChan <- &msg
+		}
+	}()
+
+	// doing this never closes the stream
+	for {
+		select {
+		case <-msgStream.Context().Done():
+
+			log.Printf("--------- %v Left Chat-------", ch.SendersName)
+			for i, element := range s.channel[ch.Name] {
+				if element == msgChannel {
+					s.channel[ch.Name] = append(s.channel[ch.Name][:i], s.channel[ch.Name][i+1:]...)
+					break
+				}
+			}
+			msg := chatpb.Message{
+				Channel: &chatpb.Channel{
+					Name:        ch.Name,
+					SendersName: ch.SendersName},
+				Message: "4040",
+				Sender:  ch.SendersName,
+			}
+
+			go func() {
+				streams := s.channel[msg.Channel.Name]
+				for _, msgChan := range streams {
+					msgChan <- &msg
+				}
+			}()
+
+			return nil
+		case msg := <-msgChannel:
+			log.Printf("Sent mes: %v", msg)
+
+			go msgStream.Send(msg)
+
+		}
+	}
+}
+
+func (s *server) SendMessage(msgStream chatpb.ChatService_SendMessageServer) error {
+	msg, err := msgStream.Recv() //receive message
+
+	if err == io.EOF {
+		fmt.Printf("done?")
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	ack := chatpb.MessageAck{Status: "SENT"}
+	msgStream.SendAndClose(&ack) //sends back it is acknowledged - only used for log right now
+
+	go func() {
+		streams := s.channel[msg.Channel.Name]
+		for _, msgChan := range streams {
+			msgChan <- msg
+		}
+	}()
+
+	return nil
+}
+
+func newServer() *server {
+	s := &server{
+		channel: make(map[string][]chan *chatpb.Message),
+	}
+	return s
 }
 
 func main() {
-	// Create listener tcp on port 9080
-	list, err := net.Listen("tcp", ":9080")
+	fmt.Println("--- SERVER APP ---")
+	lis, err := net.Listen("tcp", "localhost:9100")
 	if err != nil {
-		log.Fatalf("Failed to listen on port 9080: %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
-	grpcServer := grpc.NewServer()
-	pb.RegisterGetCurrentTimeServer(grpcServer, &Server{})
 
-	if err := grpcServer.Serve(list); err != nil {
-		log.Fatalf("failed to server %v", err)
-	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	chatpb.RegisterChatServiceServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
